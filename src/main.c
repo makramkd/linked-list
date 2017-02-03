cce14c0d43f27e662557e3adcf4e432c1e9c6a9b
Makram Kamaleddine <makram@500px.com>
2017-02-03 16:14:31 -0500

    more tests

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@ int main( int argc, char **argv )
     // test insert
     add_to_list(head, 6);
     add_to_list(head, 7);
+    add_to_list(head, 8);
     printf("The value of head is %i\n", head->val);
     printf("The value of head->next is %i\n", head->next->val);
     printf("The value of head->next->next is %i\n", head->next->next->val);
@@ -19,12 +20,16 @@ int main( int argc, char **argv )
     struct node *the_other_node = search_list(head, 8);
     printf("The search yields: %s\n", the_other_node == NULL ? "null" : "not null");
 
+    // test delete middle
+    struct node *after_deleted_1 = delete_from_list(head, 6);
+    printf("The node after 6 is: %i\n", after_deleted_1->val);
+
     // test delete head
     struct node *after_deleted = delete_from_list(head, 5);
     printf("The node after 5 is: %i\n", after_deleted->val);
 
     // test delete last node
-    struct node *other = delete_from_list(after_deleted, 7);
+    struct node *other = delete_from_list(after_deleted, 8);
     printf("The node after the last is: %s\n", other == NULL ? "null" : "not null");
     return 0;
 }
